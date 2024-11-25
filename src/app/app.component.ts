ea6dd8cc025fb24fc3592e7ed9d335c1306bb511
esouthwick <esouthwick@opentext.com>
2024-11-25 12:49:05 -0700

    added contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import {IntroComponent} from "./intro/intro.component";
 import {AboutMeComponent} from "./about-me/about-me.component";
 import {SkillsComponent} from "./skills/skills.component";
+import {ContactComponent} from "./contact/contact.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, IntroComponent, AboutMeComponent, SkillsComponent],
+  imports: [CommonModule, NavbarComponent, IntroComponent,
+    AboutMeComponent, SkillsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 @Component({
-  selector: 'app-contact',
-  standalone: true,
-  imports: [CommonModule],
+  selector: 'contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  name = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  message = new FormControl('');
 
+  contactForm: FormGroup;
+
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+    });
+  }
+
+  submitForm() {}
 }
