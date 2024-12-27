19d4f3b29ede387f15a30b837ece8390a15be47f
Evan <evan.southwick5@gmail.com>
2024-12-27 16:07:59 -0700

    changes to see if github pages images appear

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -19,18 +19,18 @@ export interface Tile {
 export class SkillsComponent {
 
   tiles: Tile[] = [
-    { icon: '/assets/icons/angular-icon-size_128.png', text: 'Angular', cols: 1, rows: 1 },
-    { icon: '/assets/icons/html-icon-size_128.png', text: 'HTML5', cols: 1, rows: 1 },
-    { icon: '/assets/icons/lang-css-icon-size_128.png', text: 'CSS3', cols: 1, rows: 1 },
-    { icon: '/assets/icons/typescript-icon-size_128.png', text: 'TypeScript/Javascript', cols: 1, rows: 1 },
-    { icon: '/assets/icons/intellijidea.256x256.png', text: 'IntelliJ', cols: 1, rows: 1 },
-    { icon: '/assets/icons/vscode-icon-size_128.png', text: 'VS Code', cols: 1, rows: 1 },
-    { icon: '/assets/icons/github-icon-size_128.png', text: 'Github', cols: 1, rows: 1 },
-    { icon: '/assets/icons/git-icon-size_128.png', text: 'Git', cols: 1, rows: 1 },
-    { icon: '/assets/icons/api-icon-size_128.png', text: "RESTful API's", cols: 1, rows: 1 },
-    { icon: '/assets/icons/python-icon-size_128.png', text: 'Python', cols: 1, rows: 1 },
-    { icon: '/assets/icons/java-icon-size_128.png', text: 'Java', cols: 1, rows: 1 },
-    { icon: '/assets/icons/postgresql-icon-size_128.png', text: 'Postgresql', cols: 1, rows: 1 },
+    { icon: '../assets/icons/angular-icon-size_128.png', text: 'Angular', cols: 1, rows: 1 },
+    { icon: '../assets/icons/html-icon-size_128.png', text: 'HTML5', cols: 1, rows: 1 },
+    { icon: '../assets/icons/lang-css-icon-size_128.png', text: 'CSS3', cols: 1, rows: 1 },
+    { icon: '../assets/icons/typescript-icon-size_128.png', text: 'TypeScript/Javascript', cols: 1, rows: 1 },
+    { icon: '../assets/icons/intellijidea.256x256.png', text: 'IntelliJ', cols: 1, rows: 1 },
+    { icon: '../assets/icons/vscode-icon-size_128.png', text: 'VS Code', cols: 1, rows: 1 },
+    { icon: '../assets/icons/github-icon-size_128.png', text: 'Github', cols: 1, rows: 1 },
+    { icon: '../assets/icons/git-icon-size_128.png', text: 'Git', cols: 1, rows: 1 },
+    { icon: '../assets/icons/api-icon-size_128.png', text: "RESTful API's", cols: 1, rows: 1 },
+    { icon: '../assets/icons/python-icon-size_128.png', text: 'Python', cols: 1, rows: 1 },
+    { icon: '../assets/icons/java-icon-size_128.png', text: 'Java', cols: 1, rows: 1 },
+    { icon: '../assets/icons/postgresql-icon-size_128.png', text: 'Postgresql', cols: 1, rows: 1 },
   ];
   numCols = 6;
 }
